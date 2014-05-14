569fc74a9a952a65846016b79ff01746d07ea3dd
Charisse <cbarandautencio@gmail.com>
2014-05-14 21:44:26 +0800

    Sum of digits

---
+++ SumOfDigits/src/SumDigits.java
@@ -0,0 +1,16 @@
+
+public class SumDigits {
+
+	public static void main(String args[])
+	{
+	int digits[]= {2,4,1,5,8};
+	int get=0;
+	
+	for(int i=0; i<digits.length; i++){
+		get = get + digits[i];
+	}
+	
+	System.out.println(get);
+	}
+	
+}
